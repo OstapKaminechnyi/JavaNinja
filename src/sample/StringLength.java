f851848457c70acef3aa2be52bfc2fc0adf171cc
OstapKaminechnyi <ostap1599@gmail.com>
2018-12-29 23:50:28 +0200

    java strings

---
+++ src/sample/StringLength.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class StringLength {
+    public static void main (String args[]){
+        String palindrome = "dot saw i was Tod";
+        int len =  palindrome.length();
+        System.out.println("String length is " +  len);
+    }
+}
---
+++ src/sample/string.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class string {
+    public static void main(String args[]){
+        char[] helloArray = {
+                'h', 'e','l','l','o','.'
+        };
+        String helloString = new String(helloArray);
+        System.out.println(helloString);
+
+    }
+}
