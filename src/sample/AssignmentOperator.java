02422a5f2dd1fa712dfc9be4f8bd7e4cb1fea06f
OstapKaminechnyi <ostap1599@gmail.com>
2018-12-29 23:01:24 +0200

    java operators

---
+++ src/sample/AssignmentOperator.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class AssignmentOperator {
+    public static void main(String args[]){
+        int num1 = 10;
+        int num2 = 20;
+        num2 = num1;
+        System.out.println("=output: " + num2) ;
+        num2 += num1;
+        System.out.println("+=output: " + num2) ;
+        num2 -= num1;
+        System.out.println("-=output: " + num2) ;
+        num2 *= num1;
+        System.out.println("*=output: " + num2) ;
+        num2 /= num1;
+        System.out.println("/=output: " + num2) ;
+
+    }
+}
