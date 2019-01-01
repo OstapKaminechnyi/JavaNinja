e84e630c278ad3118a6194fb92d63d885b83baef
OstapKaminechnyi <ostap1599@gmail.com>
2019-01-01 23:25:31 +0200

    java Date & Time

---
+++ src/sample/DateTime.java
@@ -0,0 +1,10 @@
+package sample;
+
+import java.util.Date;
+
+public class DateTime {
+    public static void main(String args[]){
+        Date date = new Date();
+        System.out.println(date.toString());
+    }
+}
