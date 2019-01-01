7e8a4b5c77c2e6fd8f46c715b3b5c77793ebc8de
OstapKaminechnyi <ostap1599@gmail.com>
2019-01-01 22:59:49 +0200

    java arrays

---
+++ src/sample/SingleDimanArray.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class SingleDimanArray {
+    public static void main(String args[]) {
+        int iarr[] = new int[3];
+        iarr[0] = 1;
+        iarr[1] = 2;
+        iarr[2] = 3;
+        System.out.println(iarr[0]);
+
+        for (int i = 0; i < iarr.length; i = i + 1){
+        System.out.println(iarr[i]);
+
+    }}
+}
