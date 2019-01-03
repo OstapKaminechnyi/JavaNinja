93e41689c11e2bfb328f943b06d40b7df0321bc4
OstapKaminechnyi <ostap1599@gmail.com>
2019-01-03 22:55:54 +0200

    java packages

---
+++ src/mypackage/Hello.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class Hello {
+    public static void main (String args[]){
+        A a = new A();
+        a.show();
+        System.out.println("Show class A");
+    }
+}
